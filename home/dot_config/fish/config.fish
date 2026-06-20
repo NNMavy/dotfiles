15493905a25fcbe463cef4e7b5bf122b22e4fa1b
NNMavy <3441334+NNMavy@users.noreply.github.com>
2026-06-20 12:07:22 +0200

    chore: Fix dotfiles
    Signed-off-by: NNMavy <3441334+NNMavy@users.noreply.github.com>

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,4 +1,4 @@
 # Disable the fish greeting message
 set fish_greeting ""
 
-fish_config theme choose "Catppuccin Macchiato" --color-theme=dark
+fish_config theme choose catppuccin-macchiato --color-theme=dark
